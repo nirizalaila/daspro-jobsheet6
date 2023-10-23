45c6692e1e75bd43ed1767e0ae6033bf42b4a9a9
nirizalaila <nirizalailaumi1@gmail.com>
2023-10-23 15:34:28 +0700

    percobaan 1

---
+++ Pemilihan2Percobaan124.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan124 {
+    public static void main(String[] args) {
+        Scanner input24 = new Scanner(System.in);
+        int tahun;
+        System.out.print("Masukkan Tahun: ");
+        tahun = input24.nextInt();
+        if ((tahun % 4) == 0){
+            if ((tahun % 100) !=0)
+            System.out.println("Tahun Kabisat");
+         } else
+            System.out.println("Bukan Tahun Kabisat");
+    }
+}
